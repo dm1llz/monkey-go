f250281a2066bd24b166abe8891789b4daae6682
david <pc_freak@att.net>
2024-02-17 14:45:19 -0800

    Added keyword and identifier functionality

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -29,6 +29,8 @@ func (lex *Lexer) readChar() {
 func (lex *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	lex.skipWhitespace()
+
 	switch lex.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, lex.ch)
@@ -49,12 +51,39 @@ func (lex *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(lex.ch) {
+			tok.Literal = lex.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, lex.ch)
+		}
 	}
 
 	lex.readChar()
 	return tok
 }
 
+func (lex *Lexer) readIdentifier() string {
+	position := lex.position
+	for isLetter(lex.ch) {
+		lex.readChar()
+	}
+
+	return lex.input[position:lex.position]
+}
+
+func (lex *Lexer) skipWhitespace() {
+	for lex.ch == ' ' || lex.ch == '\t' || lex.ch == '\n' || lex.ch == '\r' {
+		lex.readChar()
+	}
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
